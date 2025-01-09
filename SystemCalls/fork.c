af4cdd64d4e0defcd743bb85e3ec55167c6e42b6
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2025-01-09 12:10:23 +0530

    Create fork.c

---
+++ SystemCalls/fork.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<unistd.h>
+void main() {
+ pid_t id;
+ id = fork();
+ if(id==0)
+ {
+ printf("Child Process\n");
+ printf("process id is %d\n",getpid());
+ printf("parent id is %d\n",getppid());
+ }
+ else if(id>0){
+  wait(NULL);
+ printf("Parent Process\n");
+ printf("process id is %d\n",getpid());
+ }
+else
+printf("Fork failed\n");
+}
